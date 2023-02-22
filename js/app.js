7948840f118725e7345939a0b9c64ca96ad24117
NadyaKireyeva <styyypik@gmail.com>
2023-02-22 20:39:33 +0600

    feat: delete days

--- js/app.js
+++ js/app.js
@@ -79,7 +79,7 @@ function rerenderContent(activeHabit) {
               <div class="habit__comment">${
                 activeHabit.days[index].comment
               }</div>
-              <button class="habit__delete">
+              <button class="habit__delete" onclick="deleteDay(${index})">
                 <img src="images/delete.svg" alt="Удалить день ${
                   Number(index) + 1
                 }" />
@@ -121,6 +121,21 @@ function addDays(event) {
     return habit;
   });
   form["comment"].value = "";
+  rerender(globalActiveHabitId);
+  saveData();
+}
+
+function deleteDay(index) {
+  habits = habits.map((habit) => {
+    if (habit.id === globalActiveHabitId) {
+      habit.days.splice(index, 1);
+      return {
+        ...habit,
+        days: habit.days,
+      };
+    }
+    return habit;
+  });
   rerender(globalActiveHabitId);
   saveData();
 }
